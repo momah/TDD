f052b329e0623d0067fc23a6b3cb737000f44058
alu4543 <simotenerife@hotmail.com>
2012-11-24 14:01:12 +0000

    existe un numerador y un denominador

--- lib/racional.rb
+++ lib/racional.rb
@@ -1,2 +1,13 @@
 class Racional
+  
+  include Comparable
+  include Enumerable
+  
+  def initialize(numerador, denominador)
+    	@numerador, @denominador = numerador, denominador
+  end
+  
+  attr_reader :numerador, :denominador
+  
+
 end
