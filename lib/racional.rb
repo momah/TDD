0508fc8e9156afd2a54cf6fdb6017b2650aec781
alu4543 <simotenerife@hotmail.com>
2012-11-24 14:46:42 +0000

    ya esta en su forma reducida

--- lib/racional.rb
+++ lib/racional.rb
@@ -3,11 +3,22 @@ class Racional
   include Comparable
   include Enumerable
   
-  def initialize(numerador, denominador)
-    	@numerador, @denominador = numerador, denominador
+  def initialize(num, den)
+    max = mcd(numerador, denominador)
+    @numerador, @denominador = numerador/max, denominador/max
   end
   
   attr_reader :numerador, :denominador
+
   
+  #codigo en http://fromgus.blogspot.com.es/2009/05/maximo-comun-divisor.html
+  #Ejemplo : Ej: el maximo comun divisor entre 6 y 10: mcd(6:10) = mcd(10:6) = mcd(6:4) = (4:2) = mcd(2:0). Se calcula el MCD con el resto de dividir el valor absoluto del mayor por el  menor, y nos quedamo con el resto de la division y el valor menor que teníamos
+  def mcd(a,b)
+	d = a.abs, b.abs
+	while d.min != 0
+	  d = d.min, d.max % d.min
+	end
+	return d.max
+  end
 
 end
