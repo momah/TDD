a24a54c0d29f6deeb773abf2cfcdeb234b4edb53
alu4543 <simotenerife@hotmail.com>
2012-11-24 15:00:27 +0000

    Se pueden invocar al metodo num() y  demon() para obtener el numerador y el denominador

--- lib/racional.rb
+++ lib/racional.rb
@@ -10,10 +10,8 @@ class Racional
   
   attr_reader :numerador, :denominador
 
-  
-  #codigo en http://fromgus.blogspot.com.es/2009/05/maximo-comun-divisor.html
-  #Ejemplo : Ej: el maximo comun divisor entre 6 y 10: mcd(6:10) = mcd(10:6) = mcd(6:4) = (4:2) = mcd(2:0). Se calcula el MCD con el resto de dividir el valor absoluto del mayor por el  menor, y nos quedamo con el resto de la division y el valor menor que teníamos
-  def mcd(a,b)
+
+  def mcd(a,b) #codigo en http://fromgus.blogspot.com.es/2009/05/maximo-comun-divisor.html
 	d = a.abs, b.abs
 	while d.min != 0
 	  d = d.min, d.max % d.min
@@ -21,4 +19,12 @@ class Racional
 	return d.max
   end
 
+  def num
+    	@numerador
+  end
+  
+  def denom
+    	@denominador
+  end
+ 
 end
